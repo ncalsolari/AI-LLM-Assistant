bfc8b58d9a62f0296aae9d09bbe3706dbd941b72
ncalsolari <ncalsolari@gmail.com>
2026-01-02 14:08:57 -0300

    add current date/time tool

--- agent.py
+++ agent.py
@@ -1,32 +1,35 @@
 from langchain_openai import ChatOpenAI
 from langchain.agents import initialize_agent, AgentType
-from tools import calculator_tool
+from tools import calculator_tool, datetime_tool
 
 SYSTEM_PROMPT = """
 You are an AI assistant.
 
 You can answer questions using your own knowledge.
-You have access to ONLY ONE tool called Calculator.
+You have access to ONLY TWO tools called Calculator and CurrentDateTime.
 
 Use the Calculator tool ONLY when the user asks a mathematical question
 that requires a numeric calculation.
 
-If the question is not a mathematical expressions
+Use the CurrentDateTime tool ONLYwhen the user asks questions about the current time or date.
+
+If the question is not a mathematical expressions or current date/time question
 DO NOT use any tool and answer directly.
 """
 
 def create_agent():
     llm = ChatOpenAI(
-        model="gpt-3.5-turbo",
-        temperature=0
+        model= "gpt-3.5-turbo",
+        temperature= 0
     )
 
+    TOOLS = [calculator_tool,datetime_tool]
     agent = initialize_agent(
-        tools=[calculator_tool],
-        llm=llm,
-        agent=AgentType.OPENAI_FUNCTIONS,
-        verbose=True,
-        agent_kwargs={
+        tools= TOOLS,
+        llm= llm,
+        agent= AgentType.OPENAI_FUNCTIONS,
+        verbose= True,
+        agent_kwargs= {
             "system_message": SYSTEM_PROMPT
         }
     )
--- tools.py
+++ tools.py
@@ -1,4 +1,5 @@
 from langchain.tools import Tool
+from datetime import datetime
 
 
 def calculator(expression: str) -> str:
@@ -12,10 +13,34 @@ def calculator(expression: str) -> str:
     except Exception:
         return "Erro ao calcular a expressão"
 
+
 calculator_tool = Tool(
-    name="Calculator",
-    func=calculator,
-    description=(
+    name= "Calculator",
+    func= calculator,
+    description= (
         "Use this tool to solve mathematical expressions. The input must be a valid expression, e.g., '128 * 46'."
     ),
 )
+
+
+def current_datetime(date: str = "") -> str:
+    """
+    Returns the current date and time.
+    """
+
+    now = datetime.now()
+    return now.strftime("%Y-%m-%d %H:%M:%S")
+
+
+datetime_tool = Tool(
+    name="CurrentDateTime",
+    func=current_datetime,
+    description=(
+        "Use this tool for any question related to the current date or time,including today, yesterday, tomorrow, or the current day of the week."
+    ),
+)
+
+
+
+
+
